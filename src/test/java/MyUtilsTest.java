3cb6539a61166d1a04f636fcd255388403dea4d7
online-marathon <61456363+online-marathon@users.noreply.github.com>
2025-02-24 11:59:28 +0200

    Update MyUtilsTest.java

--- src/test/java/MyUtilsTest.java
+++ src/test/java/MyUtilsTest.java
@@ -1,78 +1,93 @@
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.Collections;
 import java.util.List;
 
 public class MyUtilsTest {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-		//System.out.println(DBTest.checkStub());
-		System.out.println("TypePresent - " + DBTest.isTypePresent("MyUtils"));
-		System.out.println("TypeClass - " + DBTest.isTypeClass("MyUtils"));
-		//
-		System.out.println("createConnection - " + DBTest.isMethodPresent("MyUtils", "createConnection", new Class<?>[] { }, Connection.class));
-		System.out.println("closeConnection - " + DBTest.isMethodPresent("MyUtils", "closeConnection", new Class<?>[] { }, void.class));
-		System.out.println("createStatement - " + DBTest.isMethodPresent("MyUtils", "createStatement", new Class<?>[] { }, Statement.class));
-		System.out.println("closeStatement - " + DBTest.isMethodPresent("MyUtils", "closeStatement", new Class<?>[] { }, void.class));
-		System.out.println("createSchema - " + DBTest.isMethodPresent("MyUtils", "createSchema", new Class<?>[] { String.class }, void.class));
-		System.out.println("dropSchema - " + DBTest.isMethodPresent("MyUtils", "dropSchema", new Class<?>[] { }, void.class));
-		System.out.println("useSchema - " + DBTest.isMethodPresent("MyUtils", "useSchema", new Class<?>[] { }, void.class));
-		System.out.println("createTableRoles - " + DBTest.isMethodPresent("MyUtils", "createTableRoles", new Class<?>[] { }, void.class));
-		System.out.println("createTableDirections - " + DBTest.isMethodPresent("MyUtils", "createTableDirections", new Class<?>[] { }, void.class));
-		System.out.println("createTableProjects - " + DBTest.isMethodPresent("MyUtils", "createTableProjects", new Class<?>[] { }, void.class));
-		System.out.println("createTableEmployee - " + DBTest.isMethodPresent("MyUtils", "createTableEmployee", new Class<?>[] { }, void.class));
-		System.out.println("dropTable - " + DBTest.isMethodPresent("MyUtils", "dropTable", new Class<?>[] { String.class }, void.class));
-		System.out.println("insertTableRoles - " + DBTest.isMethodPresent("MyUtils", "insertTableRoles", new Class<?>[] { String.class }, void.class));
-		System.out.println("insertTableDirections - " + DBTest.isMethodPresent("MyUtils", "insertTableDirections", new Class<?>[] { String.class }, void.class));
-		System.out.println("insertTableProjects - " + DBTest.isMethodPresent("MyUtils", "insertTableProjects", new Class<?>[] { String.class, String.class }, void.class));
-		System.out.println("insertTableEmployee - " + DBTest.isMethodPresent("MyUtils", "insertTableEmployee", new Class<?>[] { String.class, String.class, String.class }, void.class));
-		System.out.println("getRoleId - " + DBTest.isMethodPresent("MyUtils", "getRoleId", new Class<?>[] { String.class }, int.class));
-		System.out.println("getDirectionId - " + DBTest.isMethodPresent("MyUtils", "getDirectionId", new Class<?>[] { String.class }, int.class));
-		System.out.println("getProjectId - " + DBTest.isMethodPresent("MyUtils", "getProjectId", new Class<?>[] { String.class }, int.class));
-		System.out.println("getEmployeeId - " + DBTest.isMethodPresent("MyUtils", "getEmployeeId", new Class<?>[] { String.class }, int.class));
-		System.out.println("getAllRoles - " + DBTest.isMethodPresent("MyUtils", "getAllRoles", new Class<?>[] { }, List.class));
-		System.out.println("getAllDirestion - " + DBTest.isMethodPresent("MyUtils", "getAllDirestion", new Class<?>[] { }, List.class));
-		System.out.println("getAllProjects - " + DBTest.isMethodPresent("MyUtils", "getAllProjects", new Class<?>[] { }, List.class));
-		System.out.println("getAllEmployee - " + DBTest.isMethodPresent("MyUtils", "getAllEmployee", new Class<?>[] { }, List.class));
-		System.out.println("getAllDevelopers - " + DBTest.isMethodPresent("MyUtils", "getAllDevelopers", new Class<?>[] { }, List.class));
-		System.out.println("getAllJavaProjects - " + DBTest.isMethodPresent("MyUtils", "getAllJavaProjects", new Class<?>[] { }, List.class));
-		System.out.println("getAllJavaDevelopers - " + DBTest.isMethodPresent("MyUtils", "getAllJavaDevelopers", new Class<?>[] { }, List.class));
-		//
-		//
-		System.out.println("checkCreateConnection - " + DBTest.checkCreateConnection());
-		System.out.println("checkCloseConnection - " + DBTest.checkCloseConnection());
-		System.out.println("checkCreateStatement - " + DBTest.checkCreateStatement());
-		System.out.println("checkCloseStatement - " + DBTest.checkCloseStatement());
-		System.out.println("checkCreateSchema - " + DBTest.checkCreateSchema());
-		System.out.println("checkDropSchema - " + DBTest.checkDropSchema());
-		System.out.println("checkUseSchema - " + DBTest.checkUseSchema());
-		//
-		System.out.println("checkCreateTableRoles - " + DBTest.checkCreateTableRoles());
-		System.out.println("checkCreateTableDirections - " + DBTest.checkCreateTableDirections());
-		System.out.println("checkCreateTableProjects - " + DBTest.checkCreateTableProjects());
-		System.out.println("checkCreateTableEmployee - " + DBTest.checkCreateTableEmployee());
-		//
-		System.out.println("checkDropTable - " + DBTest.checkDropTable());
-		System.out.println("checkInsertTableRoles - " + DBTest.checkInsertTableRoles());
-		System.out.println("checkInsertTableDirections - " + DBTest.checkInsertTableDirections());
-		System.out.println("checkInsertTableProjects - " + DBTest.checkInsertTableProjects());
-		System.out.println("checkInsertTableEmployee - " + DBTest.checkInsertTableEmployee());
-		//
-		System.out.println("checkGetRoleId - " + DBTest.checkGetRoleId());
-		System.out.println("checkGetDirectionId - " + DBTest.checkGetDirectionId());
-		System.out.println("checkGetProjectId - " + DBTest.checkGetProjectId());
-		System.out.println("checkGetEmployeeId - " + DBTest.checkGetEmployeeId());
-		//
-		System.out.println("checkGetAllRoles - " + DBTest.checkGetAllRoles());
-		System.out.println("checkGetAllDirestion - " + DBTest.checkGetAllDirestion());
-		System.out.println("checkGetAllProjects - " + DBTest.checkGetAllProjects());
-		System.out.println("checkGetAllEmployee - " + DBTest.checkGetAllEmployee());
-		//
-		System.out.println("checkGetAllDevelopers - " + DBTest.checkGetAllDevelopers());
-		System.out.println("checkGetAllJavaProjects - " + DBTest.checkGetAllJavaProjects());
-		System.out.println("checkGetAllJavaDevelopers - " + DBTest.checkGetAllJavaDevelopers());
-		// */
-		//
-	}
+        String sb = "===== Checking MyUtils class presence =====\n" +
+                "Class found: " + DBTest.isTypePresent("MyUtils") + "\n" +
+                "Is it a class: " + DBTest.isTypeClass("MyUtils") + "\n\n" +
+                "===== Checking method presence =====\n" +
+                checkMethod("createConnection", new Class<?>[]{}, Connection.class) +
+                checkMethod("closeConnection", new Class<?>[]{}, void.class) +
+                checkMethod("createStatement", new Class<?>[]{}, Statement.class) +
+                checkMethod("closeStatement", new Class<?>[]{}, void.class) +
+                checkMethod("createSchema", new Class<?>[]{String.class}, void.class) +
+                checkMethod("dropSchema", new Class<?>[]{}, void.class) +
+                checkMethod("useSchema", new Class<?>[]{}, void.class) +
+                checkMethod("createTableRoles", new Class<?>[]{}, void.class) +
+                checkMethod("createTableDirections", new Class<?>[]{}, void.class) +
+                checkMethod("createTableProjects", new Class<?>[]{}, void.class) +
+                checkMethod("createTableEmployees", new Class<?>[]{}, void.class) +
+                checkMethod("dropTable", new Class<?>[]{String.class}, void.class) +
+                checkMethod("insertTableRoles", new Class<?>[]{String.class}, void.class) +
+                checkMethod("insertTableDirections", new Class<?>[]{String.class}, void.class) +
+                checkMethod("insertTableProjects", new Class<?>[]{String.class, String.class}, void.class) +
+                checkMethod("insertTableEmployees", new Class<?>[]{String.class, String.class, String.class}, void.class) +
+                checkMethod("getRoleId", new Class<?>[]{String.class}, int.class) +
+                checkMethod("getDirectionId", new Class<?>[]{String.class}, int.class) +
+                checkMethod("getProjectId", new Class<?>[]{String.class}, int.class) +
+                checkMethod("getEmployeeId", new Class<?>[]{String.class}, int.class) +
+                checkMethod("getAllRoles", new Class<?>[]{}, List.class) +
+                checkMethod("getAllDirections", new Class<?>[]{}, List.class) +
+                checkMethod("getAllProjects", new Class<?>[]{}, List.class) +
+                checkMethod("getAllEmployees", new Class<?>[]{}, List.class) +
+                checkMethod("getAllDevelopers", new Class<?>[]{}, List.class) +
+                checkMethod("getAllJavaProjects", new Class<?>[]{}, List.class) +
+                checkMethod("getAllJavaDevelopers", new Class<?>[]{}, List.class) +
+                "\n" +
+                "===== Checking method execution =====\n" +
+                checkExecution("createConnection", DBTest.checkCreateConnection()) +
+                checkExecution("closeConnection", DBTest.checkCloseConnection()) +
+                checkExecution("createStatement", DBTest.checkCreateStatement()) +
+                checkExecution("closeStatement", DBTest.checkCloseStatement()) +
+                checkExecution("createSchema", DBTest.checkCreateSchema()) +
+                checkExecution("dropSchema", DBTest.checkDropSchema()) +
+                checkExecution("useSchema", DBTest.checkUseSchema()) +
+                "\n" +
+                "===== Checking table creation =====\n" +
+                checkExecution("createTableRoles", DBTest.checkCreateTableRoles()) +
+                checkExecution("createTableDirections", DBTest.checkCreateTableDirections()) +
+                checkExecution("createTableProjects", DBTest.checkCreateTableProjects()) +
+                checkExecution("createTableEmployees", DBTest.checkCreateTableEmployees()) +
+                "\n" +
+                "===== Checking data insertion =====\n" +
+                checkExecution("insertTableRoles", DBTest.checkInsertTableRoles()) +
+                checkExecution("insertTableDirections", DBTest.checkInsertTableDirections()) +
+                checkExecution("insertTableProjects", DBTest.checkInsertTableProjects()) +
+                checkExecution("insertTableEmployees", DBTest.checkInsertTableEmployees()) +
+                "\n" +
+                "===== Checking ID retrieval =====\n" +
+                checkExecution("getRoleId", DBTest.checkGetRoleId()) +
+                checkExecution("getDirectionId", DBTest.checkGetDirectionId()) +
+                checkExecution("getProjectId", DBTest.checkGetProjectId()) +
+                checkExecution("getEmployeeId", DBTest.checkGetEmployeeId()) +
+                "\n" +
+                "===== Checking data retrieval =====\n" +
+                checkExecution("getAllRoles", DBTest.checkGetAllRoles()) +
+                checkExecution("getAllDirections", DBTest.checkGetAllDirections()) +
+                checkExecution("getAllProjects", DBTest.checkGetAllProjects()) +
+                checkExecution("getAllEmployees", DBTest.checkGetAllEmployees()) +
+                checkExecution("getAllDevelopers", DBTest.checkGetAllDevelopers()) +
+                checkExecution("getAllJavaProjects", DBTest.checkGetAllJavaProjects()) +
+                checkExecution("getAllJavaDevelopers", DBTest.checkGetAllJavaDevelopers());
 
+        System.out.println(sb);
+    }
+
+    /**
+     * Helper method to check if a method is present in the class.
+     */
+    private static String checkMethod(String methodName, Class<?>[] parameters, Class<?> returnType) {
+        return String.format("%-30s -> %s%n", methodName,
+                DBTest.isMethodPresent("MyUtils", methodName, parameters, returnType) ? "✔ Present" : "✘ Missing");
+    }
+
+    /**
+     * Helper method to check if a test execution is successful.
+     */
+    private static String checkExecution(String testName, boolean result) {
+        return String.format("%-30s -> %s%n", testName, result ? "✔ Passed" : "✘ Failed");
+    }
 }
